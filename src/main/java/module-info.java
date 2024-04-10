af4bb138fe038f832894635dbad81ca3adca02f9
Mysticpasta1 <ethana5tc@outlook.com>
2024-04-10 10:43:32 -0500

    ahhhhh

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.mystic.pcg {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.mystic.playingcardgame to javafx.fxml;
-    exports com.mystic.playingcardgame;
-}
